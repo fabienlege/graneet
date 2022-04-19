05020f7b4b79e704c5a30ebc33df50b05fb6a04e
Fabien LEGE <fabien.lege@ho5group.com>
2022-04-19 14:30:13 +0200

    City Service ready for cities importation in database

---
+++ back/src/city/city.service.spec.ts
@@ -0,0 +1,89 @@
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken, TypeOrmModule } from '@nestjs/typeorm';
+import dbConfig from '../config/db.config';
+import { Repository } from 'typeorm';
+import { City } from './city.entity';
+import { CityService } from './city.service';
+import typeorm = require('typeorm')
+
+describe('CityService', () => {
+  let service: CityService;
+
+  //TypeORM Mocks 
+  const insert = jest.fn().mockReturnThis()
+  const into = jest.fn().mockReturnThis()
+  const values = jest.fn().mockReturnThis()
+  const orUpdate = jest.fn().mockReturnThis()
+  const execute = jest.fn().mockReturnThis()
+  const createQueryBuilder = jest.fn().mockReturnValue({
+    insert,
+    into,
+    values,
+    orUpdate,
+    execute,
+  } as unknown as typeorm.QueryBuilder<City>);
+
+  const find = jest.fn().mockReturnValue([
+    { cityCode: '123', name: 'Paris', postalCode: '75000' },
+    { cityCode: '321', name: 'Bayonne', postalCode: '64100' }
+  ])
+
+  /**
+   * Initialise module
+   */
+  beforeAll(async () => {
+
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [CityService, {
+        provide: getRepositoryToken(City),
+        useFactory: () => ({
+          createQueryBuilder,
+          find
+        })
+      }],
+      imports: [ConfigModule]
+    }).compile();
+
+    service = module.get<CityService>(CityService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+
+  })
+
+  it("should be insert cities from gouv api", async () => {
+    // Import must return true if import succeed
+    expect(await service.syncCities()).toBe(true);
+
+    /**
+     * Check if queryBuilder was called correctly
+     * The number of times the function was called is equal to the number of cities in the gouv api response
+     */
+    expect(insert).toHaveBeenCalledTimes(35700);
+    expect(into).toHaveBeenCalledTimes(35700);
+    expect(values).toHaveBeenCalledTimes(35700);
+    expect(orUpdate).toHaveBeenCalledTimes(35700);
+    expect(execute).toHaveBeenCalledTimes(35700);
+
+  })
+
+  it("Should retrive some cities", async () => {
+    /**
+     * Get must return a list of 10 firsts cities.
+     * Here there are only 2 cities due to the mocked find function at the top of this file.
+     */
+    const cities = await service.getFirstsCities();
+    expect(find).toBeCalledTimes(1);
+    expect(cities).toHaveLength(2);
+    expect(cities[0].name).toBe('Paris');
+    expect(cities[0].postalCode).toBe('75000');
+    expect(cities[0].cityCode).toBe('123');
+
+    expect(cities[1].name).toBe('Bayonne');
+    expect(cities[1].postalCode).toBe('64100');
+    expect(cities[1].cityCode).toBe('321');
+
+  });
+});
---
+++ back/src/city/city.service.ts
@@ -0,0 +1,65 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { InjectRepository } from '@nestjs/typeorm';
+import axios from 'axios';
+import { getConnection, Repository } from 'typeorm';
+import { City } from './city.entity';
+import { IApiCity } from './interfaces/IApiCity';
+import { ICity } from './interfaces/ICity';
+
+@Injectable()
+export class CityService {
+  constructor(private configService: ConfigService, @InjectRepository(City) private cityRepository: Repository<City>) { }
+
+  /**
+   * Return the 10 firsts cities from the database.
+   * This method is used for the tests.
+   * @returns {ICity[]} list of cities
+   */
+  async getFirstsCities(): Promise<ICity[]> {
+    return this.cityRepository.find({ order: { name: 'ASC' }, take: 10 });
+  }
+
+  /**
+   * Retrive the full city list online from the gouv api and insert it into the database
+   */
+  async syncCities(): Promise<boolean> {
+    return new Promise((resolve, reject) => {
+      // Verify if JSON_DATASET_URL is defined in the config file
+      if (!this.configService.get('JSON_DATASET_URL')) {
+        throw new Error('JSON_DATASET_URL is not defined in the config file');
+      }
+
+      // Retrieve the full city list online from the gouv api
+      axios.get(this.configService.get('JSON_DATASET_URL')).then(response => {
+        // Retrieve cities from api response
+        const cities = response.data as IApiCity[];
+
+        // Convert IApiCities to ICity
+        const cityEntities: ICity[] = cities.map(city => ({ cityCode: city.codeCommune, name: city.nomCommune, postalCode: city.codePostal } as ICity));
+
+        // Prepare a promises array to strore the insert queries promises
+        const transactions = new Array<Promise<any>>();
+
+        // Insert or update cities into the database
+        cityEntities.forEach(city => {
+          transactions.push(
+            this.cityRepository.createQueryBuilder()
+              .insert()
+              .into(City)
+              .values(city)
+              .orUpdate({ conflict_target: ['cityCode'], overwrite: ['postalCode', 'name'] })
+              .execute()
+          )
+        });
+
+        // wait for all the promises to be resolved
+        Promise.all(transactions).then(() => {
+          resolve(true);
+        }).catch(reject)
+      })
+        .catch(reject);
+
+    });
+  }
+}
