0dedd32b8177d8ef202d81099bc70700fb5fc8db
Fabien LEGE <fabien.lege@ho5group.com>
2022-04-19 14:30:38 +0200

    City interfaces

---
+++ back/src/city/interfaces/ICity.ts
@@ -0,0 +1,27 @@
+/**
+ * The city object stored in database and returned to the client
+ */
+export interface ICity {
+  /**
+   * Internal database id
+   * Could be null if the city is not stored in the database
+   */
+  id?: string;
+
+  /**
+   * City name
+   */
+  name: string;
+
+  /**
+   * City postal code
+   * @example 75000
+   */
+  postalCode: string;
+
+  /**
+   * French city code
+   * @exeample 10002
+   */
+  cityCode: string;
+}
