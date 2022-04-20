b3598cee19b5f42ad9eadf68ae3bce4aad3953ea
Fabien LEGE <fabien.lege@ho5group.com>
2022-04-20 16:11:14 +0200

    Indexing components

---
+++ front/src/Components/index.ts
@@ -0,0 +1,3 @@
+export { default as SearchBarWidget } from './SearchBarWidget/SearchBarWidget';
+export { default as ResultWidget } from './ResultWidget/ResultWidget';
+export { default as Alert } from './Alert/Alert';
